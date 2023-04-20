1bb6cb4c07cc40af29486de9b74de2c39402395a
Nikita Terentyev <nterentyev@ozon.ru>
2023-04-20 10:12:44 +0400

    added copyright

--- accumulator.go
+++ accumulator.go
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023.
+ *
+ * Developed thanks to Nikita Terentyev (nar10z). Use it for good, and let your code work without problems!
+ */
+
 package go_events_accumulator
 
 import (
