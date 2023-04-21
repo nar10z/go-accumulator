b58b403b4b5e9afbdfc1bde41cc17a60b944982f
Nikita Terentyev <nterentyev@ozon.ru>
2023-04-21 09:25:52 +0400

    revert

--- storage_types.go
+++ storage_types.go
@@ -1,5 +1,3 @@
-//go:build !go1.20
-
 /*
  * Copyright (c) 2023.
  *
@@ -22,6 +20,8 @@ const (
 	List
 	// Slice storage using slice
 	Slice
+	// StdList storage using container/list
+	StdList
 )
 
 type iStorage[T comparable] interface {
@@ -37,6 +37,8 @@ func buildStorage[T comparable](st StorageType, flushSize int) iStorage[T] {
 		return storage.NewStorageSinglyList[*eventExtended[T]](flushSize)
 	case Slice:
 		return storage.NewStorageSlice[*eventExtended[T]](flushSize)
+	case StdList:
+		return storage.NewStorageList[*eventExtended[T]](flushSize)
 	default:
 		return storage.NewStorageChannel[*eventExtended[T]](flushSize)
 	}
