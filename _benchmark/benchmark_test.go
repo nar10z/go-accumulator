546d501598f8488cc5bc1811c2a13259c0baa713
Nikita Terentyev <nekit.nar10z@gmail.com>
2023-12-08 20:34:11 +0400

    fixed benchmarks

--- _benchmark/benchmark_test.go
+++ _benchmark/benchmark_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	flushSize     = 10000
+	flushSize     = 1000
 	flushInterval = time.Second
 )
 
@@ -23,18 +23,16 @@ func Benchmark_accum(b *testing.B) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	b.Run("#1 go-accumulator, slice async", func(b *testing.B) {
+	b.Run("go-accumulator, async", func(b *testing.B) {
 		summary := 0
 
-		b.ResetTimer()
-		accumulator, _ := goaccum.New[*Data](flushSize, flushInterval, func(events []*Data) error {
+		accumulator, _ := goaccum.New[Data](flushSize, flushInterval, func(events []Data) error {
 			summary += len(events)
-			time.Sleep(time.Microsecond)
 			return nil
 		})
 
 		for i := 0; i < b.N; i++ {
-			_ = accumulator.AddAsync(ctx, &Data{i: i})
+			_ = accumulator.AddAsync(ctx, Data{i: i})
 		}
 
 		accumulator.Stop()
@@ -43,13 +41,33 @@ func Benchmark_accum(b *testing.B) {
 			b.Fail()
 		}
 	})
-	b.Run("#2 go-accumulator, slice sync", func(b *testing.B) {
+	b.Run("lrweck/accumulator", func(b *testing.B) {
 		summary := 0
 
-		b.ResetTimer()
-		accumulator, _ := goaccum.New[*Data](flushSize, flushInterval, func(events []*Data) error {
+		inputChan := make(chan Data, flushSize)
+		batch := acc.New(inputChan, flushSize, flushInterval)
+
+		go func() {
+			for i := 0; i < b.N; i++ {
+				inputChan <- Data{i: i}
+			}
+			close(inputChan)
+		}()
+
+		_ = batch.Accumulate(ctx, func(o acc.CallOrigin, items []Data) {
+			summary += len(items)
+		})
+
+		if summary != b.N {
+			b.Fail()
+		}
+	})
+
+	b.Run("go-accumulator, sync", func(b *testing.B) {
+		summary := 0
+
+		accumulator, _ := goaccum.New[Data](flushSize, flushInterval, func(events []Data) error {
 			summary += len(events)
-			time.Sleep(time.Microsecond)
 			return nil
 		})
 
@@ -57,7 +75,7 @@ func Benchmark_accum(b *testing.B) {
 		errGr.SetLimit(flushSize)
 		for i := 0; i < b.N; i++ {
 			errGr.Go(func() error {
-				return accumulator.AddSync(ctx, &Data{i: i})
+				return accumulator.AddSync(ctx, Data{i: i})
 			})
 		}
 
@@ -68,56 +86,30 @@ func Benchmark_accum(b *testing.B) {
 			b.Fail()
 		}
 	})
-	b.Run("#3 go-accumulator, slice", func(b *testing.B) {
+	b.Run("go-accumulator", func(b *testing.B) {
 		summary := 0
 		n1 := b.N / 2
 		n2 := b.N - n1
 
-		b.ResetTimer()
-		accumulator, _ := goaccum.New[*Data](flushSize, flushInterval, func(events []*Data) error {
+		accumulator, _ := goaccum.New[Data](flushSize, flushInterval, func(events []Data) error {
 			summary += len(events)
-			time.Sleep(time.Microsecond)
 			return nil
 		})
 
 		for i := 0; i < n1; i++ {
-			_ = accumulator.AddAsync(ctx, &Data{i: i})
+			_ = accumulator.AddAsync(ctx, Data{i: i})
 		}
 
 		var errGr errgroup.Group
 		errGr.SetLimit(flushSize)
 		for i := 0; i < n2; i++ {
 			errGr.Go(func() error {
-				return accumulator.AddSync(ctx, &Data{i: i})
+				return accumulator.AddSync(ctx, Data{i: i})
 			})
 		}
 
 		_ = errGr.Wait()
 		accumulator.Stop()
-
-		if summary != b.N {
-			b.Fail()
-		}
-	})
-	b.Run("#4. lrweck/accumulator", func(b *testing.B) {
-		b.ResetTimer()
-
-		summary := 0
-
-		inputChan := make(chan *Data, flushSize)
-		batch := acc.New(inputChan, flushSize, flushInterval)
-
-		go func() {
-			for i := 0; i < b.N; i++ {
-				inputChan <- &Data{i: i}
-			}
-			close(inputChan)
-		}()
-
-		_ = batch.Accumulate(ctx, func(o acc.CallOrigin, items []*Data) {
-			summary += len(items)
-			time.Sleep(time.Microsecond)
-		})
 
 		if summary != b.N {
 			b.Fail()
